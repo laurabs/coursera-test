7a592e956345d1749802e728158316e71f136288
Laura <laurabbss@gmail.com>
2020-04-24 16:48:49 +0100

    final version solution

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -34,7 +34,7 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-(function (window) {
+(function () {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
@@ -43,9 +43,9 @@ WARNING!!! WARNING!!!
 // 'speak' method.
 // See Lecture 50, part 1
 
-for (var i = 0; i < names.length; i++/* fill in parts of the 'for' loop to loop over names array */) {
+for (var i = 0; i < names.length; i++) {
   
-  var firstLetter = i.charAt(0).toLowerCase();
+  var firstLetter = names[i].charAt(0).toLowerCase();
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -62,11 +62,11 @@ for (var i = 0; i < names.length; i++/* fill in parts of the 'for' loop to loop 
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   
-  if (firstLetter == "j") {
-    byeSpeaker.speak;
+  if (firstLetter === "j") {
+    byeSpeaker.speak(names[i]);
   } else {
-    helloSpeaker.speak;
+    helloSpeaker.speak(names[i]);
   }
 }
 
-})(window);
+})();
