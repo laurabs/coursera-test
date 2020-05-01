ccf6856f23e39c7b396cffff1a0405465ba86e3b
Laura <laurabbss@gmail.com>
2020-05-01 18:12:50 +0100

    Code completed v2

--- mod5-solution/js/script.js
+++ mod5-solution/js/script.js
@@ -101,10 +101,9 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
       var chosenCategoryShortName = chooseRandomCategory (categories);
 
-
+      
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
@@ -116,18 +115,15 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      // var homeHtmlToInsertIntoMainPage = ....
       var homeHtmlToInsertIntoMainPage = homeHtml;
       var randomCategoryShortName = chosenCategoryShortName.short_name;
       homeHtmlToInsertIntoMainPage =
       insertProperty(homeHtmlToInsertIntoMainPage, "randomCategoryShortName", randomCategoryShortName);
       console.log(homeHtmlToInsertIntoMainPage);
-
-
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      // ....
+ 
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
     },
@@ -141,6 +137,8 @@ function chooseRandomCategory (categories) {
   var randomArrayIndex = Math.floor(Math.random() * categories.length);
 
   // return category object with that randomArrayIndex
+  //console.log(categories[randomArrayIndex]);
+  //console.log(categories[randomArrayIndex].short_name);
   return categories[randomArrayIndex];
 }
 
